078126a44be629a4ec6fcd96bf6c10247a4d9d74
milklot <vbiostflex@gmail.com>
2021-02-01 20:11:39 -0500

    added context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
+import React, { useContext, useState } from "react";
 import './App.css';
-import Login from "./components/Login/Login"
-import SignUp from "./components/SignUp"
+
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp";
+import EventContext, { EventProvider } from "./contexts/EventContext";
+import { UserProvider } from "./contexts/UserContext";
 
 function App() {
+  
+  const event = useContext(EventContext);
+  const [user, setUser] = useState({
+    username: localStorage.getItem("username"),
+    user_id: localStorage.getItem("user_id")
+  });
+
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <EventProvider value={event}>
+      <UserProvider value={{user, setUser}}>
+        <div className="App">
+        </div>
+      </UserProvider>
+    </EventProvider>
   );
 }
 
