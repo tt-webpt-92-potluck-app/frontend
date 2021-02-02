4035111d931cbee6fb9e596e54bdd83651b78da7
BrittanyCanty <bcanty92@gmail.com>
2021-02-01 20:40:50 -0600

    add nav

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Login from "./components/Login/Login"
 import SignUp from "./components/SignUp"
+import Navigation from "./components/Navigation/Navigation";
+import EventContext, { EventProvider } from "./contexts/EventContext";
+import { UserProvider } from "./contexts/UserContext";
 
 function App() {
-  return (
-    <div className="App">
-      <Login/>
-    </div>
-  );
-}
 
-export default App;
+	const event = useContext(EventContext);
+	const [user, setUser] = useState({
+	  username: localStorage.getItem("username"),
+	  user_id: localStorage.getItem("user_id")
+	});
+
+	return (
+	  <EventProvider value={event}>
+		<UserProvider value={{user, setUser}}>
+		  <div className="App">
+		  </div>
+		</UserProvider>
+	  </EventProvider>
+	);
+  }
+
+  export default App;
+
---
+++ src/components/navagation/navagation.js
@@ -0,0 +1,74 @@
+import React, { useState, useContext } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Navigation.css";
+import UserContext from "../../contexts/UserContext";
+import Logo from"..\photos\kachori-logo-kota-chaat-food-paneer-butter-masala.jpg"
+
+const Navigation = props => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { user } = useContext(UserContext);
+  const toggle = () => setIsOpen(!isOpen);
+
+  // console.log(user.user_id);
+  return (
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/Home">
+        <img src={Logo} alt="Potluck Logo" className="navLogo" />
+      </NavbarBrand>
+
+      <NavbarToggler onClick={toggle} />
+
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="">Calendar</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="">Events</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="">Users</NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Account
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                <NavLink href="/SignUp">SignUp</NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink href="/">Login</NavLink>
+              </DropdownItem>
+              <DropdownItem>
+             <NavLink href={`/Users/${user.user_id}/EditAccount`}>
+                  Edit Account
+                </NavLink>
+              </DropdownItem>
+              {/* <DropdownItem divider /> */}
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+
+export default Navigation;
