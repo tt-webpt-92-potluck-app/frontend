5ccf46f9207f31f59f99563e56c7093d0ad301a6
Amanda Nelson <amanda.gaylord@gmail.com>
2021-02-03 22:38:39 -0500

    form updated

---
+++ src/components/OrganizerView.js
@@ -0,0 +1,15 @@
+import React from "react";
+import EventCard from "./Potlucks/EventCard";
+import NewEvent from './Potlucks/NewEventForm';
+
+const OrganizerView = () => {
+  return (
+    <>
+      <p>Organizer View</p>
+      <EventCard />
+      <NewEvent />
+    </>
+  );
+};
+
+export default OrganizerView;
---
+++ src/components/Potlucks/NewEventForm.js
@@ -0,0 +1,102 @@
+import react, { useState, useEffect } from "react";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import axios from "axios";
+
+const initialFormValues = {
+  firstName: "",
+  lastName: "",
+  name: "",
+  location: "",
+  time: "",
+  date: "",
+};
+const initialPotluckEvents = [];
+
+const NewEventForm = () => {
+  const [potlucks, setPotlucks] = useState(initialFormValues);
+  const [formValues, setFormValues] = useState(initialPotluckEvents);
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPotluck = {
+      name: formValues.eventTitle,
+      firstName: formValues.firstName,
+      lastName: formValues.lastName,
+      location: formValues.location,
+      time: formValues.time,
+      date: formValues.date,
+    };
+    setPotlucks([...potlucks, newPotluck]);
+    setFormValues(initialFormValues);
+  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="eventTitle">Potluck Name</Label>
+        <Input
+          type="text"
+          name="eventTitle"
+          value={potlucks.eventTitle}
+          onChange={handleChange}
+          required
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="firstName">Your First Name</Label>
+        <Input
+          type="text"
+          name="firstName"
+          value={potlucks.firstName}
+          onChange={handleChange}
+          required
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="lastName">Your Last Name</Label>
+        <Input
+          type="text"
+          name="lastName"
+          value={potlucks.lastName}
+          onChange={handleChange}
+          required
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="location">Location of the Event</Label>
+        <Input
+          type="text"
+          name="location"
+          value={potlucks.location}
+          onChange={handleChange}
+          required
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="date">Day of Event</Label>
+        <Input
+          type="date"
+          name="date"
+          value={potlucks.date}
+          onChange={handleChange}
+          required
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="time">Time of Event</Label>
+        <Input
+          type="time"
+          name="time"
+          value={potlucks.time}
+          onChange={handleChange}
+          required
+        />
+      </FormGroup>
+    </Form>
+  );
+};
+export default NewEventForm;
