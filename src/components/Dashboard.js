6d990dd1f4e8dfd57e76ec05f4aafcf34cc50fc3
Amanda Nelson <amanda.gaylord@gmail.com>
2021-02-06 11:46:56 -0500

    blank Dashboard component created for linking Private Route

---
+++ src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+import GuestView from "./GuestView";
+import OrganizerView from "./OrganizerView";
+
+const Dashboard = (props) => {
+  return <>Internal Dashboard Test</>;
+};
+
+export default Dashboard;
