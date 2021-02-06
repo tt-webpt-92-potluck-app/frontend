4318634db34b06b8e4f48cd0664c79645ce81b2d
Amanda Nelson <amanda.gaylord@gmail.com>
2021-02-06 12:32:45 -0500

    Dashboard layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import GuestView from "./GuestView";
 import OrganizerView from "./OrganizerView";
+import NewEventForm from "./Potlucks/NewEventForm";
 
+//need to GET users and events from the server and then create the rules to direct if statements to the GuestView and OrganizerView.
+//Then the state will be mapped to props for those components to use.
 const Dashboard = (props) => {
-  return <>Internal Dashboard Test</>;
+    const getUsers = () => {
+
+    }
+const [events, setEvents] = useState([]);
+useEffect(() => {
+    const id = 
+})
+  return (
+      <div className="internalDash">
+          <div className="dashNav">
+          <h2>Find a Potluck:</h2>
+          <button>Guest Page</button>
+          </div>
+          <div className="dashNav">
+          <h3>Edit your Potluck Event:</h3>
+          <button>Event Organizer Page</button>
+          </div>
+          <h2>Create a New PotLuck:</h2>
+          <NewEventForm />
+      </div>
+  );
 };
 
 export default Dashboard;
