31a1234fd60f9aedfc8dd97253de3b3240776370
Amanda Nelson <amanda.gaylord@gmail.com>
2021-02-07 15:05:29 -0500

    collapse added for New Event Form

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,31 +1,53 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 import GuestView from "./GuestView";
 import OrganizerView from "./OrganizerView";
 import NewEventForm from "./Potlucks/NewEventForm";
-import EventContext from '../contexts/EventContext';
-import UserContext from '../contexts/UserContext';
+import EventContext from "../contexts/EventContext";
+import UserContext from "../contexts/UserContext";
+import { Collapse } from "reactstrap";
+import axios from "axios";
 
 //need to GET users and events from the server (EventContext & UserContext) and then create the rules to direct if statements to the GuestView and OrganizerView.
 //Then the state will be mapped to props for those components to use.
-const Dashboard = (props) => {
-const [events, setEvents] = useState([]);
-useEffect(() => {
-    const id = 
-})
+function Dashboard() {
+  const username = localStorage.getItem("username");
+  console.log(`Username: ${username}`);
+  const [events, setEvents] = useState([]);
+
+  const getData = () => {
+    axiosWithAuth()
+      .get("/api/events")
+      .then((res) => {
+        console.log(`Dashboard response: ${res.data}`);
+        setEvents(res.data);
+      })
+      .catch((err) => {
+        console.log(`Error on Dashboard GET data: ${err.response}`);
+      });
+  };
+  const getUserInfo = () => {};
+
+  //collapsable "Add New Event" Form:
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-      <div className="internalDash">
-          <div className="dashNav">
-          <h2>Find a Potluck:</h2>
-          <button>Guest Page</button>
-          </div>
-          <div className="dashNav">
-          <h3>Edit your Potluck Event:</h3>
-          <button>Event Organizer Page</button>
-          </div>
-          <h2>Create a New PotLuck:</h2>
-          <NewEventForm />
+    <div className="internalDash">
+      <div className="dashNav">
+        <h2>Find a Potluck:</h2>
+        <button>Guest Page</button>
       </div>
+      <div className="dashNav">
+        <h3>Edit your Potluck Event:</h3>
+        <button>Event Organizer Page</button>
+      </div>
+      <button onClick={toggle}>Create a New Potluck</button>
+      <Collapse isOpen={isOpen}>
+        <NewEventForm />
+      </Collapse>
+    </div>
   );
-};
+}
 
 export default Dashboard;
